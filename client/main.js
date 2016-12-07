ddde0667a8a7a818c33afdd7724ea58f03db86e4
Florence <flo.cretenoud@gmail.com>
2016-12-07 15:50:11 +0000

    added arrows
    arrows are clickable and change the image shown.

--- client/main.js
+++ client/main.js
@@ -3,12 +3,28 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.Gallery.onCreated(function() {
-    Session.set('selectedImage', 'house');
-});
 Template.Menu.helpers({
     isSelected(item) {
         return Session.equals('selectedImage', item) ? 'selected' : '';
+    },
+    menuClass(item) {
+        switch(Session.get('selectedImage')) {
+            case 'house':
+                return item == 'car' || item == 'lamp' || item == 'horse' || item == 'stone' ? 'right' : 'left';
+                break;
+            case 'car':
+                return item == 'lamp' || item == 'horse' || item == 'stone' ? 'right' : 'left';
+                break;
+            case 'lamp':
+                return item == 'horse' || item == 'stone' ? 'right' : 'left';
+                break;
+            case 'horse':
+                return item == 'stone' ? 'right' : 'left';
+                break;
+            case 'stone':
+                return item !== 'stone' ? 'left' : 'right';
+                break;
+        }
     }
 })
 
@@ -50,5 +66,46 @@ Template.Menu.events({
     },
     'click #stone'(event) {
         Session.set('selectedImage', 'stone');
+    }
+});
+
+Template.Gallery.events({
+    'click .arrow-left'(event) {
+        switch(Session.get('selectedImage')) {
+            case 'house':
+                Session.set('selectedImage', 'stone');
+                break;
+            case 'car':
+                Session.set('selectedImage', 'house');
+                break;
+            case 'lamp':
+                Session.set('selectedImage', 'car');
+                break;
+            case 'horse':
+                Session.set('selectedImage', 'lamp');
+                break;
+            case 'stone':
+                Session.set('selectedImage', 'horse');
+                break;
+        }
+    },
+    'click .arrow-right'(event) {
+        switch(Session.get('selectedImage')) {
+            case 'house':
+                Session.set('selectedImage', 'car');
+                break;
+            case 'car':
+                Session.set('selectedImage', 'lamp');
+                break;
+            case 'lamp':
+                Session.set('selectedImage', 'horse');
+                break;
+            case 'horse':
+                Session.set('selectedImage', 'stone');
+                break;
+            case 'stone':
+                Session.set('selectedImage', 'house');
+                break;
+        }
     }
 });
